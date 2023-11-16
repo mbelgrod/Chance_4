00e655f597ce6de683af3af0fc87bb234c9794d3
mbelgrod <mbelgrod@risd.edu>
2023-11-16 07:48:22 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,203 @@
+// Array of image URLs
+const imageArray = [
+    'images/NF_GroundhogDay-03.svg',
+    'images/NF_GroundhogDay-04.svg',
+    'images/NF_GroundhogDay-05.svg',
+    'images/NF_GroundhogDay-06.svg',
+    'images/NF_GroundhogDay-07.svg',
+    'images/NF_GroundhogDay-08.svg',
+    'images/NF_GroundhogDay-09.svg',
+    'images/NF_GroundhogDay-10.svg',
+    'images/NF_GroundhogDay-11.svg',
+    'images/NF_GroundhogDay-12.svg',
+    'images/NF_GroundhogDay-13.svg',
+    'images/NF_GroundhogDay-14.svg',
+    'images/NF_GroundhogDay-15.svg',
+    'images/NF_GroundhogDay-16.svg',
+    'images/NF_GroundhogDay-17.svg',
+    'images/NF_GroundhogDay-18.svg',
+    'images/NF_GroundhogDay-19.svg',
+    'images/NF_GroundhogDay-20.svg',
+    'images/NF_GroundhogDay-21.svg',
+    'images/NF_GroundhogDay-22.svg',
+    'images/NF_GroundhogDay-23.svg',
+    'images/NF_GroundhogDay-24.svg',
+    'images/NF_GroundhogDay-25.svg',
+    'images/NF_GroundhogDay-26.svg',
+    'images/NF_GroundhogDay-27.svg',
+    'images/NF_GroundhogDay-28.svg',
+    'images/NF_GroundhogDay-29.svg',
+    'images/NF_GroundhogDay-30.svg',
+    'images/NF_GroundhogDay-31.svg',
+    'images/NF_GroundhogDay-32.svg',
+    'images/NF_GroundhogDay-33.svg',
+    'images/NF_GroundhogDay-34.svg',
+    'images/NF_GroundhogDay-35.svg',
+    'images/NF_GroundhogDay-36.svg',
+    'images/NF_GroundhogDay-37.svg',
+    'images/NF_GroundhogDay-38.svg',
+    'images/NF_GroundhogDay-39.svg',
+    'images/NF_GroundhogDay-40.svg',
+    'images/NF_GroundhogDay-47.svg',
+    'images/NF_GroundhogDay-48.svg',
+    'images/NF_GroundhogDay-49.svg',
+    'images/NF_GroundhogDay-50.svg',
+    'images/NF_GroundhogDay-51.svg',
+    'images/NF_GroundhogDay-52.svg',
+    'images/NF_GroundhogDay-53.svg',
+    'images/NF_GroundhogDay-54.svg',
+    'images/NF_GroundhogDay-55.svg',
+    'images/NF_GroundhogDay-56.svg',
+    'images/NF_GroundhogDay-57.svg',
+    'images/NF_GroundhogDay-58.svg',
+
+    'images/NF_GroundhogDay-77.svg',
+    'images/NF_GroundhogDay-78.svg',
+    'images/NF_GroundhogDay-79.svg',
+    'images/NF_GroundhogDay-80.svg',
+    'images/NF_GroundhogDay-81.svg',
+    'images/NF_GroundhogDay-82.svg',
+
+    'images/NF_GroundhogDay-89.svg',
+    'images/NF_GroundhogDay-90.svg',
+    'images/NF_GroundhogDay-91.svg',
+    'images/NF_GroundhogDay-92.svg',
+    'images/NF_GroundhogDay-93.svg',
+    'images/NF_GroundhogDay-94.svg',
+    'images/NF_GroundhogDay-95.svg',
+    'images/NF_GroundhogDay-96.svg',
+    'images/NF_GroundhogDay-97.svg',
+    'images/NF_GroundhogDay-98.svg',
+    'images/NF_GroundhogDay-99.svg',
+    'images/NF_GroundhogDay-100.svg',
+    'images/NF_GroundhogDay-101.svg',
+    'images/NF_GroundhogDay-102.svg',
+    'images/NF_GroundhogDay-103.svg',
+    'images/NF_GroundhogDay-104.svg',
+    'images/NF_GroundhogDay-105.svg',
+    'images/NF_GroundhogDay-106.svg',
+    'images/NF_GroundhogDay-107.svg',
+    'images/NF_GroundhogDay-108.svg',
+    'images/NF_GroundhogDay-109.svg',
+    'images/NF_GroundhogDay-110.svg',
+    'images/NF_GroundhogDay-111.svg',
+    'images/NF_GroundhogDay-112.svg',
+    'images/NF_GroundhogDay-113.svg',
+    'images/NF_GroundhogDay-114.svg',
+    'images/NF_GroundhogDay-115.svg',
+    'images/NF_GroundhogDay-116.svg',
+    'images/NF_GroundhogDay-117.svg',
+    'images/NF_GroundhogDay-118.svg',
+    'images/NF_GroundhogDay-119.svg',
+    'images/NF_GroundhogDay-120.svg',
+    'images/NF_GroundhogDay-121.svg',
+    'images/NF_GroundhogDay-122.svg',
+    'images/NF_GroundhogDay-123.svg',
+    'images/NF_GroundhogDay-124.svg',
+    'images/NF_GroundhogDay-125.svg',
+    'images/NF_GroundhogDay-126.svg',
+    'images/NF_GroundhogDay-127.svg',
+    'images/NF_GroundhogDay-128.svg',
+    'images/NF_GroundhogDay-129.svg',
+    'images/NF_GroundhogDay-130.svg',
+    'images/NF_GroundhogDay-131.svg',
+    'images/NF_GroundhogDay-132.svg',
+    'images/NF_GroundhogDay-133.svg',
+    'images/NF_GroundhogDay-134.svg',
+    'images/NF_GroundhogDay-135.svg',
+    'images/NF_GroundhogDay-136.svg',
+    'images/NF_GroundhogDay-137.svg',
+    'images/NF_GroundhogDay-138.svg',
+    'images/NF_GroundhogDay-139.svg',
+    'images/NF_GroundhogDay-140.svg',
+    'images/NF_GroundhogDay-141.svg',
+    'images/NF_GroundhogDay-142.svg',
+    'images/NF_GroundhogDay-143.svg',
+    'images/NF_GroundhogDay-144.svg',
+    'images/NF_GroundhogDay-145.svg',
+    'images/NF_GroundhogDay-146.svg',
+    'images/NF_GroundhogDay-147.svg',
+    'images/NF_GroundhogDay-148.svg',
+    'images/NF_GroundhogDay-149.svg',
+    'images/NF_GroundhogDay-150.svg',
+    'images/NF_GroundhogDay-151.svg',
+    'images/NF_GroundhogDay-152.svg',
+    'images/NF_GroundhogDay-153.svg',
+    'images/NF_GroundhogDay-154.svg',
+    'images/NF_GroundhogDay-155.svg',
+    'images/NF_GroundhogDay-156.svg',
+    'images/NF_GroundhogDay-157.svg',
+    'images/NF_GroundhogDay-158.svg',
+   
+    'images/NF_GroundhogDay-269.svg',
+    'images/NF_GroundhogDay-270.svg',
+    'images/NF_GroundhogDay-271.svg',
+    'images/NF_GroundhogDay-272.svg',
+    'images/NF_GroundhogDay-273.svg',
+    'images/NF_GroundhogDay-274.svg',
+    'images/NF_GroundhogDay-275.svg',
+    'images/NF_GroundhogDay-276.svg',
+    'images/NF_GroundhogDay-277.svg',
+    'images/NF_GroundhogDay-278.svg',
+    'images/NF_GroundhogDay-279.svg',
+    'images/NF_GroundhogDay-280.svg',
+    'images/NF_GroundhogDay-281.svg',
+    'images/NF_GroundhogDay-282.svg',
+    'images/NF_GroundhogDay-283.svg',
+    'images/NF_GroundhogDay-284.svg',
+    'images/NF_GroundhogDay-285.svg',
+    'images/NF_GroundhogDay-286.svg',
+    'images/NF_GroundhogDay-287.svg',
+    'images/NF_GroundhogDay-288.svg',
+    'images/NF_GroundhogDay-289.svg',
+    'images/NF_GroundhogDay-290.svg',
+    'images/NF_GroundhogDay-291.svg',
+    'images/NF_GroundhogDay-292.svg',
+    'images/NF_GroundhogDay-293.svg',
+    'images/NF_GroundhogDay-294.svg',
+    'images/NF_GroundhogDay-295.svg',
+    'images/NF_GroundhogDay-296.svg',
+    'images/NF_GroundhogDay-297.svg',
+    'images/NF_GroundhogDay-298.svg',
+    'images/NF_GroundhogDay-299.svg',
+    'images/NF_GroundhogDay-300.svg',
+    'images/NF_GroundhogDay-301.svg',
+    'images/NF_GroundhogDay-302.svg',
+
+    // ... (add more image URLs as needed)
+];
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.addEventListener('mousemove', placeImage);
+});
+
+function placeImage(event) {
+    const randomImage = getRandomImage();
+
+    // Create image element
+    const imageElement = document.createElement('img');
+    imageElement.src = randomImage;
+
+    const scale = .5;
+
+    // Set the scale by adjusting width and height
+    imageElement.width = imageElement.width * scale;
+    imageElement.height = imageElement.height * scale;
+
+    // Set a random rotation angle between 0 and 360 degrees
+    const rotation = Math.random() * 360;
+
+    // Set the position, rotation, and color of the image based on the mouse move event
+    imageElement.style.position = 'absolute';
+    imageElement.style.left = `${event.clientX - imageElement.width / 2}px`;
+    imageElement.style.top = `${event.clientY - imageElement.height / 2}px`;
+    imageElement.style.transform = `rotate(${rotation}deg)`;
+
+    // Append the image to the body
+    document.body.appendChild(imageElement);
+}
+
+function getRandomImage() {
+    const randomIndex = Math.floor(Math.random() * imageArray.length);
+    return imageArray[randomIndex];
+}
